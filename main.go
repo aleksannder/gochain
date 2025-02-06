4daceed5277b0c546675bfee8dbcaf76f4c369ef
Aleksander Zajac <121639741+aleksannder@users.noreply.github.com>
2025-02-06 18:11:35 +0100

    added basic bolt k/v store and cli for printing chain and adding new blocks (#3)

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/aleksannder/gochain/domain"
-	"strconv"
-
 )
 
 func main() {
 
 	bc := domain.NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Zeko")
-	bc.AddBlock("Send 3 BTC to Zeko")
+	defer bc.Db.Close()
 
-	for _, block := range bc.Blocks {
-		fmt.Printf("Previous hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("Data: %s\n", block.Data)
-		pow := domain.NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := domain.CLI{Bc: bc}
+	cli.Run()
 }
